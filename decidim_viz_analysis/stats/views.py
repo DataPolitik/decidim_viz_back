85ceebe9d7babf4387f9246e0c7e5bdbaf406e9b
jorgechp <jorgechp@uoc.edu>
2022-06-09 14:41:07 +0200

    Return dictionary instead of dataframe

--- decidim_viz_analysis/stats/views.py
+++ decidim_viz_analysis/stats/views.py
@@ -1,7 +1,9 @@
 import itertools
+import json
+
 import pandas as pd
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from stats.models import Proposal, User
 
 from stats.phi.phi import __compute_phi
@@ -14,16 +16,19 @@ def index(request):
 def endorsements(request):
     threshold = 1
     list_of_proposals = Proposal.objects.values_list('id_proposal', flat=True)[:100]
-    list_of_users = User.objects.values_list('id', flat=True)
+    list_of_users = User.objects.values_list('id', flat=True)[:100]
     dataframe_proposals = pd.DataFrame(0, columns=list_of_users, index=list_of_proposals, dtype='int16')
-    dataframe_users = pd.DataFrame(0, columns=list_of_users, index=list_of_users, dtype='float')
+    dict_users = dict()
+    for user in list_of_users:
+        dict_users[user] = dict()
 
     for proposal in list_of_proposals:
         users = User.objects.filter(supports__id_proposal=proposal)
         for user in users:
-            dataframe_proposals[user.id][proposal] = dataframe_proposals[user.id][proposal] + 1
+            if user.id in list_of_users:
+                dataframe_proposals[user.id][proposal] = dataframe_proposals[user.id][proposal] + 1
 
-    for user_couple in set(itertools.combinations(dataframe_users, 2)):
+    for user_couple in set(itertools.combinations(list_of_users, 2)):
         user_a = user_couple[0]
         user_b = user_couple[1]
 
@@ -31,6 +36,7 @@ def endorsements(request):
         proposals_b = dataframe_proposals[user_b]
         phi, t = __compute_phi(proposals_a, proposals_b)
         if abs(t) > threshold:
-            dataframe_users[user_a][user_b] = phi
+            dict_users[user_a][user_b] = phi
+            dict_users[user_b][user_a] = phi
 
-    return HttpResponse(dataframe_users)
+    return JsonResponse(dict_users)
