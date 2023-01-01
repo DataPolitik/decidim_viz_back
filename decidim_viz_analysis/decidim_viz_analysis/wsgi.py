4689b9822d91eb767c228029c91391b753987ef3
jorgechp <jorgechp@uoc.edu>
2023-01-01 21:35:01 +0100

    modify wsgi

--- decidim_viz_analysis/decidim_viz_analysis/wsgi.py
+++ decidim_viz_analysis/decidim_viz_analysis/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decidim_viz_analysis.settings')
+sys.path.append('/home/jorge/decidim_vis/decidim_viz_back/decidim_viz_analysis')
+os.environ['DJANGO_SETTINGS_MODULE'] = "decidim_viz_analysis.settings"
 
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decidim_viz_analysis.settings')
+
+os.environ.setdefault("LANG", "en_US.UTF-8")
+os.environ.setdefault("LC_ALL", "en_US.UTF-8")
+
+#activamos nuestro virtualenv
+activate_this = "/home/jorge/decidim_vis/decidim_viz_back/venv/bin/activate_this.py"
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+#obtenemos la aplicación
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
